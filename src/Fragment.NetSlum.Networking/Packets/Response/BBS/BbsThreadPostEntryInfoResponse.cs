d6312f9dda2f86da4cfa1ba8b011a9f5f826bf37
Zero1UP <Zero1UP@gmail.com>
2023-06-09 23:14:07 -0400

    Fixed issues with bbsthreadDetails

--- src/Fragment.NetSlum.Networking/Packets/Response/BBS/BbsThreadPostEntryInfoResponse.cs
+++ src/Fragment.NetSlum.Networking/Packets/Response/BBS/BbsThreadPostEntryInfoResponse.cs
@@ -2,7 +2,6 @@ using Fragment.NetSlum.Core.Buffers;
 using Fragment.NetSlum.Core.Extensions;
 using Fragment.NetSlum.Networking.Constants;
 using Fragment.NetSlum.Networking.Objects;
-
 namespace Fragment.NetSlum.Networking.Packets.Response.BBS;
 
 public class BbsThreadPostDetailResponse : BaseResponse
@@ -48,16 +47,15 @@ public class BbsThreadPostDetailResponse : BaseResponse
 
         return this;
     }
-
     public override FragmentMessage Build()
     {
-        uint unk0 = _postId;
+        uint unk1 = 0;
         uint unk2 = 0;
-        var unk3 = new Span<byte>(new byte[96]);
+        var unk3 = new Span<byte>(new byte[46]);
 
         var usernameBytes = _postedByUsername.MaxSubstring(15).ToShiftJis().EnsureSize(16);
         var titleBytes = _postTitle.MaxSubstring(31).ToShiftJis().EnsureSize(32);
-        var titlePreview = _postTitle.MaxSubstring(16).ToShiftJis().EnsureSize(17);
+        var titlePreview = _postTitle.MaxSubstring(16).ToShiftJis().EnsureSize(18);
 
         var writer = new MemoryWriter(sizeof(uint) * 4 +
                                       usernameBytes.Length +
@@ -66,13 +64,15 @@ public class BbsThreadPostDetailResponse : BaseResponse
                                       unk3.Length
         );
 
-        writer.Write(unk0); // This appears to be whether or not the user has read the post or not. (If value >0 then 'NEW' does not appear)
+        writer.Write(_postId); //If this number isn't unique in a thread then only the latest post is going to show for the whole thread.
         writer.Write(_postId);
         writer.Write(unk2);
         writer.Write((uint)_postedAtDate.ToEpoch());
         writer.Write(usernameBytes);
         writer.Write(titlePreview);
+       
         writer.Write(unk3);
+        var test = writer.Buffer.ToArray().TakeWhile((v, index) => writer.Buffer.ToArray().Skip(index).Any(w => w != 0x00)).ToArray().Length;
         writer.Write(titleBytes);
 
         return new FragmentMessage
