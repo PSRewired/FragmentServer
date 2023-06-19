6dfd4e27fee0d54dea8bd12188a115a5a4a5c8d4
Blake LaFleur <blackburn2929@gmail.com>
2023-06-19 10:44:21 -0500

    Add helper methods for returning single messages from handlers

--- src/Fragment.NetSlum.Networking/Packets/Request/BaseRequest.cs
+++ src/Fragment.NetSlum.Networking/Packets/Request/BaseRequest.cs
@@ -11,8 +11,16 @@ public abstract class BaseRequest
 {
     public ILogger Log => Serilog.Log.ForContext(GetType());
 
-    public virtual Task<ICollection<FragmentMessage>> GetResponse(FragmentTcpSession session, FragmentMessage request)
-    {
-        throw new NotImplementedException($"Class [{GetType().Name}] does not support this method.");
-    }
+    public abstract Task<ICollection<FragmentMessage>> GetResponse(FragmentTcpSession session, FragmentMessage request);
+
+    /// <summary>
+    /// Helper method to return a single message from a request handler
+    /// </summary>
+    /// <param name="response"></param>
+    /// <returns></returns>
+    protected Task<ICollection<FragmentMessage>> ReturnSingle(FragmentMessage response) =>
+        Task.FromResult(ReturnSingleAsync(response));
+
+    protected ICollection<FragmentMessage> ReturnSingleAsync(FragmentMessage response) =>
+        new[] { response };
 }
--- src/Fragment.NetSlum.Networking/Packets/Request/Misc/ReturnToDesktopRequest.cs
+++ src/Fragment.NetSlum.Networking/Packets/Request/Misc/ReturnToDesktopRequest.cs
@@ -13,9 +13,6 @@ public class ReturnToDesktopRequest : BaseRequest
 {
     public override Task<ICollection<FragmentMessage>> GetResponse(FragmentTcpSession session, FragmentMessage request)
     {
-        return Task.FromResult<ICollection<FragmentMessage>>(new[]
-        {
-            new ReturnToDesktopResponse().Build(),
-        });
+        return ReturnSingle(new ReturnToDesktopResponse().Build());
     }
 }
